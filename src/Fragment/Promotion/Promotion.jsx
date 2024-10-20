5f7f5633605cf7e24b2a0d63004f1e361c97cc05
bagasrr <bagasramadhan239@gmail.com>
2024-10-20 21:06:01 +0700

    aslfak

--- src/Fragment/Promotion/Promotion.jsx
+++ src/Fragment/Promotion/Promotion.jsx
@@ -9,24 +9,9 @@ const Promotion = () => {
   const minHeight = {
     minHeight: "60vh",
   };
-<<<<<<< HEAD
-  const imgBackgorund = {
-    backgroundImage: `url(/source/BgWaButton.png`,
-  };
-
-  return (
-    <div className="my-20" style={minHeight}>
-      <img src="/source/Mainbanner.png" alt="Main Banner" />
-      <Element name="kontak">
-        <div className="relative w-11/12 h-40 sm:h-96 mx-auto my-10 rounded-lg bg-cover bg-center" style={imgBackgorund}>
-          <Button type="wa" phoneNumber="81252800095" message="Halo! saya tertarik dengan Anvaya, Bolehkah saya bertanya beberapa hal?" caption="Tanya-Tanya Dulu Yukk" />
-        </div>
-      </Element>
-=======
 
   return (
     <div className="mt-24" style={minHeight}>
->>>>>>> 5b5bb2c (penambahan video)
       <Element name="category">
         <Title title="Temukan Listing Premium Kami" />
         <div className="w-screen flex flex-wrap justify-center h-full">
