8f42f33a2d1e154d0f6ce4f1c60c4d05488d2e5a
bagasrr <bagasramadhan239@gmail.com>
2024-10-22 13:24:35 +0700

    ubah posisi harga pada properti

--- src/Element/Card/Card.jsx
+++ src/Element/Card/Card.jsx
@@ -20,14 +20,14 @@ const AdvantageCard = (props) => {
 const HomeTypeCard = (props) => {
   const { imgsrc, imgalt, hometype, price } = props;
   return (
-    <div className="w-32 h-44 md:h-50 md:w-40 lg:w-1/6  lg:h-72 shadow-lg align-center justify-center bg-teal-600 rounded-lg m-5 hover:scale-105 duration-300 relative">
+    <div className="w-32 h-44 md:h-50 md:w-40 lg:w-1/6  lg:h-72 hover:shadow-lg  hover:shadow-cyan-500 align-center justify-center bg-teal-600 rounded-lg m-5 hover:scale-105 duration-300 ">
       <img src={imgsrc} alt={imgalt} className="bg-cover bg-center h-2/3 w-full" />
-      <div className="absolute top-2 right-0 bg-rose-700 text-white p-1 lg:p-3 text-right rounded-l-lg">
-        <h1 className="text-sm lg:text-lg font-bold">
-          From Rp <br /> {price} <small>Jt</small>
+      <h3 className="text-lg font-bold text-center md:p-3  text-white ">{hometype}</h3>
+      <div className=" bg-rose-700 text-white p-1 lg:p-3 text-right rounded-b-lg">
+        <h1 className="text-sm lg:text-lg ">
+          Mulai dari Rp <span className="text-xl font-bold">{price} </span> <small>Jt - an</small>
         </h1>
       </div>
-      <h3 className="text-lg font-bold text-center md:p-3 lg:p-7 text-white ">{hometype}</h3>
     </div>
   );
 };
